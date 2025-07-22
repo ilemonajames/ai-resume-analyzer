dabc3703d0d21d27b2c5f763200ce0570a3ddfb2
James Ilemona <ilemonaj@gmail.com>
2025-07-22 03:18:44 +0100

    landing page set up

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,15 +2,15 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Resumer" },
-    { name: "description", content: "Smart Feedback for your dream job" },
+    { title: "Resumelyzer" },
+    { name: "description", content: "Smart Feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <section className="main-section">
-      <div className="page-header">
+      <div className="page-heading">
         <h1>Track Your Application & Resume Ratings</h1>
         <h2>Review your submission and check feedback with AI-powered solutions</h2>
 
